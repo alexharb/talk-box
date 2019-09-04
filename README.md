9688bf52a9737b164a3c78c29c19ff689a2ff079
Alex Harb <alex.harb11@gmail.com>
2019-09-04 14:37:26 -0700

    Initialized repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# PROJECT-NAME
+
+npm install
+
+"npm run build" to build the app
+
+"npm start" to run the server with nodemon
---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+function App () {
+
+  return (
+    <div>
+      <h1>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
